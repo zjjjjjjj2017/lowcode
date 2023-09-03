43bfee4f0b6a24cc0d43151720fb663f490118f7
zhengjie <zhengjie5389@163.com>
2023-09-03 14:44:45 +0800

    updata

---
+++ .history/vue.config_20230903114502.js
@@ -0,0 +1,23 @@
+// vue.config.js
+module.exports = {
+    configureWebpack: {
+        module: {
+            rules: [
+                {
+                    test: /\.mjs$/,
+                    include: /node_modules/,
+                    type: "javascript/auto"
+                },
+            ]
+        }
+    },
+    build: {
+        // Template for index.html
+        index: path.resolve(__dirname, '/docs/index.html'),  //之前是'../dist/index.html'
+
+        // Paths
+        assetsRoot: path.resolve(__dirname, '/docs'),  // 之前是 '../dist'
+        assetsSubDirectory: 'static',
+        assetsPublicPath: '/',    // 之前是 '/'
+    }
+}
